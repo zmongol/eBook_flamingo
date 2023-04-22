5344f1076fd87aacf7c0cc9bbc52b20537af338c
zmongol <zmongolcode@gmail.com>
2023-04-22 17:04:06 +0800

    updated z52 fonts.

---
+++ lib/Component/MongolFonts.dart
@@ -0,0 +1,163 @@
+class MongolFonts {
+
+  static final String z52haratig = 'z52haratig';
+  static final String z52amglangtig = 'z52amglangtig';
+  static final String z52buduntig = 'z52buduntig';
+  static final String z52chimegtig = 'z52chimegtig';
+  static final String z52egulentig = 'z52egulentig';
+  static final String z52hawangtig = 'z52hawangtig';
+  static final String z52holsnujugtig = 'z52holsnujugtig';
+  static final String z52horqtsagaantig = 'z52horqtsagaantig';
+  static final String z52munggunujugtig = 'z52munggunujugtig';
+  static final String z52ononsansseriftig = 'z52ononsansseriftig';
+  static final String z52ordostig = 'z52ordostig';
+  static final String z52qingmingtig = 'z52qingmingtig';
+  static final String z52qimedtig = 'z52qimedtig';
+  static final String z52saitig = 'z52saitig';
+  static final String z52sanstig = 'z52sanstig';
+  static final String z52sanstigbold = 'z52sanstigbold';
+  static final String z52solibotig = 'z52solibotig';
+  static final String z52sonintig = 'z52sonintig';
+  static final String z52togtig = 'z52togtig';
+  static final String z52tsagaantig = 'z52tsagaantig';
+  static final String z52uljitig = 'z52uljitig';
+  static final String z52brahmisanstig = 'z52brahmisanstig';
+  static final String z52sidemtig = 'z52sidemtig';
+  static final String z52modonbartig = 'z52modonbartig';
+  static final String z52togoraitig = 'z52togoraitig';
+  static final String z52erihetig = 'z52erihetig';
+  static final String z52galttig = 'z52galttig';
+  static final String z52oyanggatig = 'z52oyanggatig';
+  static final String z52holosontig = 'z52holosontig';
+  static final String z52garcagtig = 'z52garcagtig';
+  static final String z52tegustig = 'z52tegustig';
+  static final String z52durugetig = 'z52durugetig';
+  static final String z52hosigatig = 'z52hosigatig';
+  static final String z52lundbtig = 'z52lundbtig';
+  static final String z52sogonagtig = 'z52sogonagtig';
+  static final String z52teniguntig = 'z52teniguntig';
+  static final String z52ujugtig = 'z52ujugtig';
+  static final String z52xanadutig = 'z52xanadutig';
+  static final String z52oigorjintig = 'z52oigorjintig';
+  static final String z52sodortig = 'z52sodortig';
+  static final String z52tatalgatig = 'z52tatalgatig';
+  static final String z52agolatig = 'z52agolatig';
+  static final String z52daicintig = 'z52daicintig';
+  static final String z52tubtiintig = 'z52tubtiintig';
+  static final String z52dardastig = 'z52dardastig';
+  static final String z52dashtsedentig = 'z52dashtsedentig';
+  static final String z52gorbannirootig = 'z52gorbannirootig';
+  static final String z52biligtig = 'z52biligtig';
+  static final String z52khalhtig = 'z52khalhtig';
+  static final String z52tatalgantig = 'z52tatalgantig';
+  static final String z52tolitig = 'z52tolitig';
+  static final String z52boldbirtig = 'z52boldbirtig';
+  static final String z52enkiragotig = 'z52enkiragotig';
+
+  static final List fontList = [
+    [z52agolatig, '  ᡥᡪᡱᡱᡭᢑᡧ ᢘᡬᡬᡨ'],
+    [z52ordostig, ' ᡥᡭᢞᢙᡭᢓ ᢘᡬᡬᡨ'],
+    [z52saitig, ' ᢔᡪᡫᡨ ᢘᡬᡬᡨ'],
+    [z52qimedtig, ' ᢚᡬᢎᡪᡭᡧ ᢘᡬᡬᡨ'],
+    [z52enkiragotig, ' ᡥᡪᢊᢊᡪᡨ ᡥᡬᢞᡪᡱᡱᡳ ᢘᡬᡬᡨ'],
+    [z52dashtsedentig, ' ᢘᡪᢔᡬᢦᡪᢙᡪᡧ ᢘᡬᡬᡨ'],
+    [z52galttig, ' ᢈᡪᢑᢙᡳ ᢘᡬᡬᡨ'],
+    [z52gorbannirootig, ' ᢈᡭᢞᡳᡪᡧ ᡯᡬᢞᡭᡱᡱᡳ ᢘᡬᡬᡨ'],
+    [z52boldbirtig, ' ᡴᡭᢑᡭᡭᡧ ᡳᡬᢝ ᢘᡬᡬᡨ'],
+    [z52oigorjintig, ' ᡥᡭᡬᡬᡱᡱᡭᢞᢛᡬᡧ ᢘᡬᡬᡨ'],
+    [z52sidemtig, ' ᢔᡬᢙᡪᢍ ᢘᡬᡬᡨ'],
+    [z52modonbartig, ' ᢌᡭᢙᡭᡧ ᡳᡪᢝ ᢘᡬᡬᡨ'],
+    [z52togoraitig, ' ᢘᡭᡱᡱᡭᢞᡪᡫ ᢘᡬᡬᡨ'],
+    [z52biligtig, ' ᡳᡬᢑᡬᡬᡨ ᢘᡬᡬᡨ'],
+    [z52khalhtig, ' ᡸᡪᢑᢇᡨ ᢘᡬᡬᡨ'],
+    [z52tatalgantig, ' ᡸᡭᢑᡭᢔᡭᡧ ᢘᡪᢙᡪᢑᢉᡨ ᢘᡬᡬᡨ'],
+    [z52tolitig, ' ᢘᡭᢑᡫ ᢘᡬᡬᡨ'],
+    [z52xanadutig, ' ᢗᡪᡪᢊᢙᡳ ᢘᡬᡬᡨ'],
+    [z52dardastig, ' ᢘᡪᢞᢙᡪᢓ ᢘᡬᡬᡨ'],
+    [z52tubtiintig, ' ᢘᡭᡬᡳᢙᡬᡬᡧ ᢘᡬᡬᡨ'],
+    [z52daicintig, ' ᢘᡪᡬᡬᢚᡬᡧ ᢘᡬᡬᡨ'],
+    [z52sodortig, ' ᢔᡭᢙᡭᢝ ᢘᡬᡬᡨ'],
+    [z52tatalgatig, ' ᢘᡪᢙᡪᢑᢉᡨ ᢘᡬᡬᡨ'],
+    [z52ujugtig, ' ᡥᡭᡬᢛᡭᡬᡨ ᢘᡬᡬᡨ'],
+    [z52lundbtig, ' ᢏᡭᡬᡪᢙᡭᡲ ᢘᡬᡬᡨ'],
+    [z52tsagaantig, '  ᢚᡪᡱᡱᡪᡧ ᢘᡬᡬᡨ'],
+    [z52erihetig, ' ᡥᢞᡬᢊᡪᡨ ᢘᡬᡬᡨ'],
+    [z52oyanggatig, ' ᡥᡭᢜᡪᡪᢊᢉᡨ ᢘᡬᡬᡨ'],
+    [z52holosontig, ' ᡸᡭᢑᡭᢔᡭᡧ ᢘᡬᡬᡨ'],
+    [z52garcagtig, ' ᢈᡪᢞᢚᡪᢇ ᢘᡬᡬᡨ'],
+    [z52tegustig, ' ᢘᡪᢋᡭᢓ ᢘᡬᡬᡨ'],
+    [z52durugetig, ' ᢘᡭᡬᢞᡭᢊᡪᡨ ᢘᡬᡬᡨ'],
+    [z52hosigatig, ' ᡸᡭᢔᡬᢉᡨ ᢘᡬᡬᡨ'],
+    [z52sogonagtig, ' ᢔᡭᡱᡱᡭᡱᡪᢇ ᢘᡬᡬᡨ'],
+    [z52teniguntig, ' ᢘᡪᡱᡬᢋᡭᡧ ᢘᡬᡬᡨ'],
+    [z52horqtsagaantig, ' ᡸᡭᢞᢚᡫ ᢚᡪᡱᡱᡪᡧ ᢘᡬᡬᡨ'],
+    [z52ononsansseriftig, ' ᡥᡭᡱᡭᡧ ᢔᡪᡪᢓ ᢘᡬᡬᡨ'],
+    [z52sanstig, ' ᢔᡪᡪᢓ ᢘᡬᡬᡨ'],
+    [z52sanstigbold, ' ᢔᡪᡪᢓ ᢘᡭᢎᡳ ᢘᡬᡬᡨ'],
+    [z52munggunujugtig, ' ᢌᡭᡬᡪᢊᢋᡭᡧ ᡥᡭᡬᢛᡭᡬᡨ ᢘᡬᡬᡨ'],
+    [z52holsnujugtig, ' ᡸᡭᢑᡭᢔᡭᡧ ᡥᡭᡬᢛᡭᡬᡨ ᢘᡬᡬᡨ'],
+    [z52chimegtig, '  ᢚᡬᢎᡪᡬᡨ ᢘᡬᡬᡨ'],
+    [z52uljitig, ' ᡥᡭᡬᢑᢛᡪᡫ ᢘᡬᡬᡨ'],
+    [z52haratig, ' ᡸᡪᢝᡨ ᢘᡬᡬᡨ'],
+    [z52amglangtig, ' ᡥᡪᢎᡭᡱᡱᡭᢑᡪᡪᡬᡨ ᢘᡬᡬᡨ'],
+    [z52togtig, ' ᢘᡭᢇ ᢘᡬᡬᡨ'],
+    [z52buduntig, ' ᡴᡭᡬᢙᡭᢋᡭᡧ ᢘᡬᡬᡨ'],
+    [z52egulentig, ' ᡥᢋᡭᢑᡪᡧ ᢘᡬᡬᡨ'],
+    [z52hawangtig, ' ᡸᡪᢟᡪᡪᡬᡨ ᢘᡬᡬᡨ'],
+    [z52qingmingtig, ' ᢚᡬᡪᡬᡨ ᢌᡬᡪᡬᡨ ᢘᡬᡬᡨ'],
+    [z52solibotig, ' ᢔᡭᢑᡳᡬᡫᡨ  ᢘᡬᡬᡨ'],
+    [z52sonintig, ' ᢔᡭᡱᡬᡧ ᢘᡬᡬᡨ'],
+
+  ];
+
+
+
+  //   static final List fontList = [
+  //   [z52agolatig, '  ᡥᡪᡱᡱᡭᢑᡧ ᢘᡬᡬᡨ'],
+  //   [z52ordostig, ' ᡥᡭᢞᢙᡭᢓ ᢘᡬᡬᡨ'],
+  //   [z52saitig, ' ᢔᡪᡫᡨ ᢘᡬᡬᡨ'],
+  //   [z52qimedtig, ' ᢚᡬᢎᡪᡭᡧ ᢘᡬᡬᡨ'],
+  //   [z52dashtsedentig, ' ᢘᡪᢔᡬᢦᡪᢙᡪᡧ ᢘᡬᡬᡨ'],
+  //   [z52galttig, ' ᢈᡪᢑᢙᡳ ᢘᡬᡬᡨ'],
+  //   [z52oigorjintig, ' ᡥᡭᡬᡬᡱᡱᡭᢞᢛᡬᡧ ᢘᡬᡬᡨ'],
+  //   [z52sidemtig, ' ᢔᡬᢙᡪᢍ ᢘᡬᡬᡨ'],
+  //   [z52modonbartig, ' ᢌᡭᢙᡭᡧ ᡳᡪᢝ ᢘᡬᡬᡨ'],
+  //   [z52togoraitig, ' ᢘᡭᡱᡱᡭᢞᡪᡫ ᢘᡬᡬᡨ'],
+  //   [z52xanadutig, ' ᢗᡪᡪᢊᢙᡳ ᢘᡬᡬᡨ'],
+  //   [z52dardastig, ' ᢘᡪᢞᢙᡪᢓ ᢘᡬᡬᡨ'],
+  //   [z52tubtiintig, ' ᢘᡭᡬᡳᢙᡬᡬᡧ ᢘᡬᡬᡨ'],
+  //   [z52daicintig, ' ᢘᡪᡬᡬᢚᡬᡧ ᢘᡬᡬᡨ'],
+  //   [z52sodortig, ' ᢔᡭᢙᡭᢝ ᢘᡬᡬᡨ'],
+  //   [z52tatalgatig, ' ᢘᡪᢙᡪᢑᢉᡨ ᢘᡬᡬᡨ'],
+  //   [z52ujugtig, ' ᡥᡭᡬᢛᡭᡬᡨ ᢘᡬᡬᡨ'],
+  //   [z52lundbtig, ' ᢏᡭᡬᡪᢙᡭᡲ ᢘᡬᡬᡨ'],
+  //   [z52tsagaantig, '  ᢚᡪᡱᡱᡪᡧ ᢘᡬᡬᡨ'],
+  //   [z52erihetig, ' ᡥᢞᡬᢊᡪᡨ ᢘᡬᡬᡨ'],
+  //   [z52oyanggatig, ' ᡥᡭᢜᡪᡪᢊᢉᡨ ᢘᡬᡬᡨ'],
+  //   [z52holosontig, ' ᡸᡭᢑᡭᢔᡭᡧ ᢘᡬᡬᡨ'],
+  //   [z52garcagtig, ' ᢈᡪᢞᢚᡪᢇ ᢘᡬᡬᡨ'],
+  //   [z52tegustig, ' ᢘᡪᢋᡭᢓ ᢘᡬᡬᡨ'],
+  //   [z52durugetig, ' ᢘᡭᡬᢞᡭᢊᡪᡨ ᢘᡬᡬᡨ'],
+  //   [z52hosigatig, ' ᡸᡭᢔᡬᢉᡨ ᢘᡬᡬᡨ'],
+  //   [z52sogonagtig, ' ᢔᡭᡱᡱᡭᡱᡪᢇ ᢘᡬᡬᡨ'],
+  //   [z52teniguntig, ' ᢘᡪᡱᡬᢋᡭᡧ ᢘᡬᡬᡨ'],
+  //   [z52horqtsagaantig, ' ᡸᡭᢞᢚᡫ ᢚᡪᡱᡱᡪᡧ ᢘᡬᡬᡨ'],
+  //   [z52ononsansseriftig, ' ᡥᡭᡱᡭᡧ ᢔᡪᡪᢓ ᢘᡬᡬᡨ'],
+  //   [z52sanstig, ' ᢔᡪᡪᢓ ᢘᡬᡬᡨ'],
+  //   [z52sanstigbold, ' ᢔᡪᡪᢓ ᢘᡭᢎᡳ ᢘᡬᡬᡨ'],
+  //   [z52munggunujugtig, ' ᢌᡭᡬᡪᢊᢋᡭᡧ ᡥᡭᡬᢛᡭᡬᡨ ᢘᡬᡬᡨ'],
+  //   [z52holsnujugtig, ' ᡸᡭᢑᡭᢔᡭᡧ ᡥᡭᡬᢛᡭᡬᡨ ᢘᡬᡬᡨ'],
+  //   [z52chimegtig, '  ᢚᡬᢎᡪᡬᡨ ᢘᡬᡬᡨ'],
+  //   [z52uljitig, ' ᡥᡭᡬᢑᢛᡪᡫ ᢘᡬᡬᡨ'],
+  //   [z52haratig, ' ᡸᡪᢝᡨ ᢘᡬᡬᡨ'],
+  //   [z52amglangtig, ' ᡥᡪᢎᡭᡱᡱᡭᢑᡪᡪᡬᡨ ᢘᡬᡬᡨ'],
+  //   [z52togtig, ' ᢘᡭᢇ ᢘᡬᡬᡨ'],
+  //   [z52buduntig, ' ᡴᡭᡬᢙᡭᢋᡭᡧ ᢘᡬᡬᡨ'],
+  //   [z52egulentig, ' ᡥᢋᡭᢑᡪᡧ ᢘᡬᡬᡨ'],
+  //   [z52hawangtig, ' ᡸᡪᢟᡪᡪᡬᡨ ᢘᡬᡬᡨ'],
+  //   [z52qingmingtig, ' ᢚᡬᡪᡬᡨ ᢌᡬᡪᡬᡨ ᢘᡬᡬᡨ'],
+  //   [z52solibotig, ' ᢔᡭᢑᡳᡬᡫᡨ  ᢘᡬᡬᡨ'],
+  //   [z52sonintig, ' ᢔᡭᡱᡬᡧ ᢘᡬᡬᡨ'],
+
+  // ];
+}
