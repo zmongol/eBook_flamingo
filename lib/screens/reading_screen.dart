5f46fc418b97acc3afb8592d1a3301ddf2c18fe1
zmongol <zmongolcode@gmail.com>
2023-04-23 20:11:26 +0800

    updating the cosmetics.

--- lib/screens/reading_screen.dart
+++ lib/screens/reading_screen.dart
@@ -3,6 +3,9 @@ import 'package:book_app/models/books/book_model.dart';
 import 'package:flutter/material.dart';
 import 'package:mongol/mongol.dart';
 
+import '../Component/MongolFonts.dart';
+import 'Donate.dart';
+
 class ReadingScreen extends StatelessWidget {
   final int chapterIndex;
   final Book book;
@@ -18,10 +21,29 @@ class ReadingScreen extends StatelessWidget {
     BookChapter chapter = book.chapters[chapterIndex];
     return Scaffold(
       appBar: AppBar(
-        title: Text(book.title),
+        title: Text(book.title,
+            style: TextStyle(
+                fontFamily: MongolFonts.z52ordostig, color: Colors.white)),
+        backgroundColor: Color.fromARGB(255, 164, 166, 168),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.favorite), //Go to Donation page
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Donation()), //Go to Donation page
+              );
+            },
+          ),
+        ],
       ),
       body: SingleChildScrollView(
-        child: MongolText(chapter.content),
+        child: MongolText(chapter.content,
+            style: TextStyle(
+                fontFamily: MongolFonts.z52ordostig,
+                color: Colors.black,
+                fontSize: 18)),
       ),
     );
   }
