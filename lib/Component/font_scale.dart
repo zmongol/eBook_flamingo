e8c1ff0fd61eb7948c3608b31944642a21482005
zmongol <zmongolcode@gmail.com>
2023-05-02 15:46:35 +0800

    font scale.

--- lib/Component/font_scale.dart
+++ lib/Component/font_scale.dart
@@ -1,5 +1,5 @@
 class FontScale {
   static const double regular = 1.0;
-  static const double medium = 1.20;
-  static const double large = 1.60;
+  static const double medium = 1.50;
+  static const double large = 2.20;
 }
